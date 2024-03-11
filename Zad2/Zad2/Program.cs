f258822eac994c7447208e424b15703fd5efabdf
jasczajka <czajka.jan12@gmail.com>
2024-03-11 18:41:05 +0100

    add working max

--- Zad2/Zad2/Program.cs
+++ Zad2/Zad2/Program.cs
@@ -5,7 +5,9 @@ class Program
     static void Main(string[] args)
     {
         int average = CalculateAverage(new int[5] { 0, 4, 6, 8, 10 });
+        int max = CalculateMax(new int[5] { 0, 4, 6, 8, 10 });
         Console.WriteLine(average);
+        Console.WriteLine(max);
     }
 
     static int CalculateAverage(int[] numbers)
@@ -18,4 +20,19 @@ class Program
 
         return sum / numbers.Length;
     }
+
+    static int CalculateMax(int[] numbers)
+    {
+        int max = numbers[0];
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+
+        return max;
+    }
+
 }
--- Zad2/Zad2/obj/Debug/net8.0/Zad2.AssemblyInfo.cs
+++ Zad2/Zad2/obj/Debug/net8.0/Zad2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zad2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+794156e714f480369cdc2efa16d16c08322b29f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d681c2e0fd86b48d0077dd9d778385bfa22b87d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zad2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zad2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
