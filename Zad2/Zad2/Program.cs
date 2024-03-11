d681c2e0fd86b48d0077dd9d778385bfa22b87d1
jasczajka <czajka.jan12@gmail.com>
2024-03-11 18:33:27 +0100

    add working average

--- Zad2/Zad2/Program.cs
+++ Zad2/Zad2/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("modyfikacja 3");
+        int average = CalculateAverage(new int[5] { 0, 4, 6, 8, 10 });
+        Console.WriteLine(average);
+    }
+
+    static int CalculateAverage(int[] numbers)
+    {
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        }
+
+        return sum / numbers.Length;
     }
 }
--- Zad2/Zad2/obj/Debug/net8.0/Zad2.AssemblyInfo.cs
+++ Zad2/Zad2/obj/Debug/net8.0/Zad2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zad2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f84de84492169b0d853a1af9d9e8ac53363a8422")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+794156e714f480369cdc2efa16d16c08322b29f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zad2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zad2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
